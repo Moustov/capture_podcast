e0e999c53a28ff87e9ef2f09d4a7c61596322133
chris <christophe.moustier@free.fr>
2023-06-26 07:46:19 +0200

    works with https://www.radiofrance.fr/franceinter/podcasts/quand-les-dieux-rodaient-sur-la-terre

--- capture_podcasts.py
+++ capture_podcasts.py
@@ -1,5 +1,46 @@
+import os
+
 import requests
 import urllib.request
+
+
+def get_serie_name(p):
+    # \"seriePath\":\"franceinter/podcasts/serie-heracles-super-heros-chez-les-grecs\",
+    s = p.split("seriePath")
+    serie = s[1].split(",")[0]
+    res = serie.split("/")[-1].replace("\\","").replace('"',"")
+    return res
+
+
+def replace_special_chars(res):
+    res = res.replace(r"\xe2\x80\x99", "'")
+    res = res.replace(r"\xc3\xa8", "è")
+    res = res.replace(r"\xc3\xa9", "é")
+    res = res.replace(r"\xc3\xaa", "ê")
+    res = res.replace(r"\xc3\xa0", "à")
+    res = res.replace(r"\xc3\xaf", "ï")
+    res = res.replace(r"\xc3\x89", "É")
+    res = res.replace(r"\xc3\x", "_")
+    res = res.replace(r"/", "_")
+    res = res.replace(r":", "_")
+    res = res.replace(r"?", "_")
+    res = res.replace(r"*", "_")
+    res = res.replace(r"<", "_")
+    res = res.replace("\\", "_")
+    res = res.replace('"', "_")
+    res = res.replace('|', "_")
+    return res
+
+
+def get_episode_name(p):
+    # \"serieEpisodeTitle\":\"6 : L’arc transmis à Philoctète\",
+    # \"episodeSerieTitle\":\"Héraclès, super-héros chez les Grecs 6/6 : L’arc transmis à Philoctète\",
+    s = p.split("episodeSerieTitle")
+    serie = s[1].split('"')[2]
+    res = serie.replace("\\\\", "")
+    res = replace_special_chars(res)
+    return res
+
 
 for page in range(1, 50):
     print(f"------ page {page} -------")
@@ -17,16 +58,12 @@ for page in range(1, 50):
         podcast = p.split(".mp3")
         if "podcast" in podcast[0]:
             url_podcast = f'https://media.radiofrance-podcast.net{podcast[0]}.mp3'
-            print(url_podcast)
-            # \"serieEpisodeTitle\":\"6 : L’arc transmis à Philoctète\",
-            # \"episodeSerieTitle\":\"Héraclès, super-héros chez les Grecs 6/6 : L’arc transmis à Philoctète\",
-            # \"seriePath\":\"franceinter/podcasts/serie-heracles-super-heros-chez-les-grecs\",
-            podcast_title = "test"
             try:
-                # r = requests.get(url_podcast, allow_redirects=True)
-                # open(f'{podcast_title}.mp3', 'wb').write(r.content)
-                urllib.request.urlretrieve(url_podcast, f'{podcast_title}.mp3')
-                # song.extract_song_from_url(url_song)
-                # song.store_song("c:/chords")
+                print(url_podcast)
+                serie = get_serie_name(p)
+                episode = get_episode_name(p)
+                if not os.path.exists(serie):
+                    os.makedirs(serie)
+                urllib.request.urlretrieve(url_podcast, f'{serie}\\{episode}.mp3')
             except Exception as e:
                 print(str(e))
